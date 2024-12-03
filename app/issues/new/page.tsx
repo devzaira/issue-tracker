1366eae2903b0dffb339c5c2a6d66d1be3e8311f
Zaira Cruz <devzaira@gmail.com>
2024-12-03 23:30:45 +0800

    Rafactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -25,6 +25,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);  
+      router.push('/issues');
+    } catch (error) {
+      setSubmitting(false);
+      setError('An unexpected error occured.');
+    }
+  });
+
   return (
     <div className='max-w-xl'>
       {error && <Callout.Root color="red" className='mb-5'>
@@ -32,16 +43,7 @@ const NewIssuePage = () => {
         </Callout.Root>}
       <form 
         className='space-y-3' 
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post('/api/issues', data);  
-            router.push('/issues');
-          } catch (error) {
-            setSubmitting(false);
-            setError('An unexpected error occured.');
-          }
-        })}>
+        onSubmit={onSubmit}>
           <TextField.Root placeholder='Title' {...register("title")}>
           </TextField.Root>
           <ErrorMessage>{errors.title?.message}</ErrorMessage>
